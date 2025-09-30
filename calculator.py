eeaa35388bdbf165dc0a36149a314a8df517d647
Maria Pore <mariapore06@gmail.com>
2025-09-30 10:39:46 +0100

    buttons

---
+++ calculator.py
@@ -0,0 +1,22 @@
+import tkinter
+
+button_values = {
+    ["AC", "+/-", "%", "÷"],
+    ["7", "8", "9", "x"],
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "√", "="],
+}
+
+right_symbols = ["÷", "×", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%"]
+
+row_count = len(button_values)
+column_count = len(button_values[0])
+
+color_light_gray = "#D4D4D2"
+color_black = "#1C1C1C"
+color_dark_gray = "#505050"
+color_orange = "#FF9500"
+color_white = "white"
+
